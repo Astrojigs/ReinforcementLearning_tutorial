f3c2a402cf4b5b590de95783f3b068f996aaf30a
Jigsy <astrojigs24@gmail.com>
2021-03-17 21:00:59 +0530

    Updated readme and main.py

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Code Description
 The code in the **main.py file**  contains description on the topic of reinforcement learning given in the Oreilly Book.
 
+The example is: Cartpole
+The observations(obs) are *position*, *velocity*, *angle of pole*, *angular velocity* resp.
+
 `env` and `obs` are from **gym**
 
 One can list all the environments possible through OpenAI gym, using;
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ env = gym.make('CartPole-v1')
 obs = env.reset()
 
 # print(obs) #[-0.04340855 -0.04825326  0.00893705  0.02182048]
-
+#obs = [position (0=center), velocity, angle of the pole (0 = vertical), angular velocity]
 '''Render the scenario'''
 # env.render()
 
@@ -12,7 +12,9 @@ obs = env.reset()
 # print(env.action_space) # Discrete(2)
 '''Discrete(2) implies that the possible actions are integers 0 or 1, which represent accelerating left(0)
 or right(1).'''
+
+#Let's accelerate the cart towards the right
 action = 1
 obs, reward, done, info = env.step(action=action)
 #(array([-0.00291647,  0.42505656, -0.04575379, -0.60914895]), 1.0, False, {})
-print(obs)
+# The step() method executes the given actions and returns four values

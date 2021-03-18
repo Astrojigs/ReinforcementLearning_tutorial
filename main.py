71b39d3b4480a7b3ecb855b4c107da7757717f96
Jigsy <astrojigs24@gmail.com>
2021-03-18 15:17:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 env = gym.make('CartPole-v1')
 obs = env.reset()
@@ -63,7 +64,7 @@ model.add(tf.keras.layers.Dense(5, activation='elu', input_shape=[n_inputs]))
 model.add(tf.keras.layers.Dense(1,activation='sigmoid'))
 
 # Function to play a step in the environment
-def play_one step(env,obs,model,loss_fn):
+def play_one_step(env,obs,model,loss_fn):
     with tf.GradientTape() as tape:
         left_proba = model(obs[np.newaxis]) #obs[np.newaxis] and obs.reshape((1,)+ obs.shape[:]) is the same
         action = (tf.random.uniform([1,1])>left_proba)
@@ -90,5 +91,50 @@ def play_multiple_episodes(env, n_episodes, n_max_steps, model, loss_fn):
         all_rewards.append(current_rewards)
         all_grads.append(current_grads)
     return all_rewards, all_grads
+
+# Function to discount
+def discount_rewards(rewards, discount_factor):
+    discounted = np.array(rewards)
+    for step in range(len(rewards) - 2, -1, -1):
+        discounted[step]+=discounted[step + 1] * discount_factor
+    return discounted
+
+# Function to normalize rewards:
+def discount_and_normalize_rewards(all_rewards, discounted_factor):
+    all_discounted_rewards = [discount_rewards(rewards, discounted_factor) for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    rewards_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [(discounted_rewards - rewards_mean)/reward_std for discounted_rewards in all_discounted_rewards]
+
+# Let us see if the function works
+# print(discount_rewards([10,0,-50], discount_factor=0.8))
+# Prints: [-22 -40 -50]
+
+n_iterations = 150
+n_episodes_per_update = 10
+n_max_steps = 200
+discount_factor = 0.95
+
+# Optmizer
+optmizer = tf.keras.optimizers.Adam(lr=0.01)
+# Loss
+loss_fn = tf.keras.losses.binary_crossentropy
+
+for iteration in range(n_iterations):
+    all_rewards,all_grads = play_multiple_episodes(env, n_episodes_per_update,n_max_steps,model, loss_fn)
+    all_final_rewards = discount_and_normalize_rewards(all_rewards, discount_factor)
+
+    all_mean_grads = []
+    for var_index in range(len(model.trainable_variables)):
+
+        mean_grads = tf.reduce_mean(
+        [final_reward * all_grads[episode_index][step][var_index]
+        for episode_index, final_rewards in enumerate(all_final_rewards)
+        for step, final_reward in enumerate(final_rewards)], axis=0
+        )
+
+        all_mean_grads.append(mean_grads)
+    optmizer.apply_gradients(zip(all_mean_grads, model.trainable_variables))
 # call close() method to free resources
 env.close()

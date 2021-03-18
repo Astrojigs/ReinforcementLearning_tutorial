209a383bd0acf46a5435d6cfefc5629a0cb57204
Jigsy <astrojigs24@gmail.com>
2021-03-18 20:53:37 +0530

    Update final.py

--- final.py
+++ final.py
@@ -8,9 +8,13 @@ obs = env.reset()
 
 model = tf.keras.models.load_model('first_model_tf_agent_cartpole.h5')
 
-action = model.predict(obs[np.newaxis])
-for i in range(100):
+action = tf.random.uniform([1,1])>model.predict(obs[np.newaxis])
+print(action)
+for i in range(1000):
+    action = tf.random.uniform([1,1])>model.predict(obs[np.newaxis])
     obs, reward, done, info = env.step(int(action[0,0]))
     env.render()
+    if done:
+        break
 
 env.close()

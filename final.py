fe5f95118592f171648637d81ef2e0c2c513d093
Jigsy <astrojigs24@gmail.com>
2021-03-18 23:17:51 +0530

    updated final.py
    The final.py is now fully working

--- final.py
+++ final.py
@@ -8,11 +8,14 @@ obs = env.reset()
 
 model = tf.keras.models.load_model('first_model_tf_agent_cartpole.h5')
 
-action = tf.random.uniform([1,1])>model.predict(obs[np.newaxis])
-print(action)
+# Some initial action given:
+action = model.predict(obs[np.newaxis])
+
+# Set arbitrary time
 for i in range(1000):
     action = tf.random.uniform([1,1])>model.predict(obs[np.newaxis])
     obs, reward, done, info = env.step(int(action[0,0]))
+    print(action)
     env.render()
     if done:
         break
